acc91b3c65f7a3828548f381838fa7207f92ff91
Elian Rezk <elian.rezk@lau.edu>
2023-01-25 20:25:38 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("Elian Rezk");
 		System.out.println("Mechatronics Engineering");
+		System.out.println("First Year");
 	}
 }
