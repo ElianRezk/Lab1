02cd5b0b002c55da388279dd195cf06d495d4410
Elian Rezk <elian.rezk@lau.edu>
2023-01-25 20:33:35 +0200

    Revert "My year of study added"
    This reverts commit 2760566535006217111f8c019023cad63377f55d.

--- Me.java
+++ Me.java
@@ -2,6 +2,5 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("Elian Rezk");
 		System.out.println("Mechatronics Engineering");
-		System.out.println("First Year");
 	}
 }
