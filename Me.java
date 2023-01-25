b94a55cc78c502f117e1d284af223bc54cf43962
Elian Rezk <elian.rezk@lau.edu>
2023-01-25 20:23:52 +0200

    My name and major

--- Me.java
+++ Me.java
@@ -1,6 +1,6 @@
 public class Me{
 	public static void main(String[] args){
 		System.out.println("Elian Rezk");
-		System.out.println("Mechatronic Engineering");
+		System.out.println("Mechatronics Engineering");
 	}
 }
